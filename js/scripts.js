7af2842c75a9c675e08da47275b08f3623f6a87d
aprulaungaung <97704610+aprulaungaung@users.noreply.github.com>
2022-01-30 20:54:44 +0000

    update the files
    updated scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+alert()
+alert('Hello World');
+
+let myFavouriteDrink='Hot Chocolate';
+document.write(myFavouriteDrink);
+let myFavouriteFood='Pizza';
+document.write(myFavouriteDrink);
+
+let numberOfEggs=12;
+document.write(numberOfEggs);
+let myAge=35;
+document.write(myAge);
+
+let yourAge=myage;
+document.write(yourAge);
+
